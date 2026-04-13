6e4ec2251a4e2709a5fc1ea10f8e233087e2da51
loo-08 <zxzxhr100@gmail.com>
2026-04-13 20:26:26 +0900

    메인 페이지 및 공통 레이아웃 구조 구현

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Header /> 
+      {/* Header 여기가 홈페이지 상단 헤더고 */}
+      {/* 아래 main 부분이 나머지 아래 부분임 */}
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
